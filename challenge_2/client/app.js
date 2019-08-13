1c22a024d406f6833b38c8d7c0029348d90e84ff
omega1984 <omegareborn2019@gmail.com>
2019-08-13 15:57:25 -0700

    just started

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,10 @@
+var app = {
+  init: function(){
+    $('#app').append('<form id="form"></form>');
+    $('#form').append('<input type="textarea" id="text">');
+    $('#form').append('<button type"submit">Submit</button>');
+  },
+  handleSubmit: function(){
 
-
+  }
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,7 @@
+// express app
+
+var express = require('express');
+var app = express();
 
 
+app.use(express.static(path.join(__dirname, './client')));
