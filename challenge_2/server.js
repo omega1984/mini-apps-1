585c30c9c0dd63e6c0d0ed86ef7cbf3b6567a8a1
omega1984 <omegareborn2019@gmail.com>
2019-08-13 19:15:09 -0700

    finished partial

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,17 @@
-// express app
-
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
+app.use(express.urlencoded());
+app.use(express.static('client'));
 
-app.use(express.static(path.join(__dirname, './client')));
+app.get('/', (req, res) =>{
+  
+})
+
+app.post('/', (req, res) => {
+  var info = req.body;
+  res.send(info);
+})
+
+app.listen(PORT, () => console.log(`PORT is listening on ${PORT}`));
